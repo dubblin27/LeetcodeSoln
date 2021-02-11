7938b586f73c39fe5b08e10bf06dc4db922209e2
Sabrish Kumar P <58567129+dubblin27@users.noreply.github.com>
2021-02-11 23:16:34 +0530

    All Soln for N_Queens

--- n_Queens.py
+++ n_Queens.py
@@ -1,11 +1,12 @@
 
-def isSafe(mat,r,c) : 
+def isSafe(mat,r,c) :
+    #to check if 2 Queens are in the same coloumn 
     for i in range(r):
         if mat[i][c] == 1:
             return False 
     
     i,j = r,c 
-
+    #to check Left diagonal '\'
     while i >= 0 and j >= 0 : 
         if mat[i][j] == 1 : 
             return False
@@ -13,7 +14,7 @@ def isSafe(mat,r,c) :
         j -=1 
     
     i,j = r,c 
-
+    #to check right diagonal '/'
     while j < N and i >= 0 :
         if mat[i][j] == 1 :
             return False
@@ -23,20 +24,19 @@ def isSafe(mat,r,c) :
     return True
 
 
-def printsoln(mat):
-    for i in range(N) : 
-        print(mat[i])
-    print()
-
 def nQueen(mat,r):
+    # if N Queens are placed = print results
     if r == N : 
-        printsoln(mat)
+        print(mat)
         return
-    
+    # Placing each queen at a position and checking if valid using recursion
     for i in range(N):
         if isSafe(mat,r,i):
+            #placing queen
             mat[r][i] = 1
+            # recurion for next row
             nQueen(mat,r+1)
+            #backtrack and cancel it
             mat[r][i] = 0
 
 N = int(input("Enter Number of Rows/Cols : "))
