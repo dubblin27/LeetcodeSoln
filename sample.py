ff3018758ffe25ab9a0caa2789299de8e3e2a184
dubblin27 <dubblin27@gmail.com>
2021-03-18 09:24:07 +0530

    ok

--- sample.py
+++ sample.py
@@ -1,42 +1,15 @@
-def shortestPathBinaryMatrix(matrix):
-        def neighbours(i,j):
-            #just incrementing to find top down left right values
-            
-            arr = []
-            
-            if i> 0 :
-                arr.append((i-1, j))
-            if i < L-1 :
-                arr.append((i+1, j))
-            if j > 0 :
-                arr.append((i, j-1))
-            if j < M-1 : 
-                arr.append((i, j+1))
-            
-            return arr
-        
-        def long_path(i,j) : 
-            #for each value the Max is initialized to 1
-            # after that max is found using recursion
-            Max = 1 
-            for x,y in neighbours(i,j):
-                if matrix[x][y] == 0 : 
-                    matrix[i][j] = -1
-                 
-                    # if there is a greater value then do this 
-                    Max = max(Max,long_path(x,y)+1)
-                    matrix[i][j] = 0
-                print(Max)
-            return Max
-        
-        if not matrix : return 0 
-        L = len(matrix) 
-        M = len(matrix[0])
-        
-        Max = 1 
-        for i in range(L) : 
-            for j in range(M) : 
-                Max = max(Max,long_path(i,j))
-        return Max
 
-print(shortestPathBinaryMatrix([[0,0],[1,0]]))
+n, pkt_limit, fine = list(map(int,input().split()))
+
+arr1 = sorted(list(map(int,input().split())))
+arr2 = sorted(list(map(int,input().split())))
+
+curr_perm = []
+curr_fine = 0
+for i in range(n):
+    curr_perm.append([arr1[i],arr2[~i]])
+for i in curr_perm :
+    if sum(i) > pkt_limit:
+        curr_fine += (sum(i) - pkt_limit)*fine 
+print(curr_fine) 
+
